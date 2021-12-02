d59b781e2f705d6a31645e3a95d417086a2bcaf2
Aluno <Aluno@D095289LABIN005>
2021-12-01 21:15:25 -0300

    Mostra valor da soma

--- supermercado/src/aulagithub/Testando.java
+++ supermercado/src/aulagithub/Testando.java
@@ -12,8 +12,15 @@ public class Testando {
 		String nome = "Jonas Monte da Silva" ;
 		int idade = 20;
 		
+		int a = 10;
+		int b = 5;
+		
+		double soma = a+b;
+		
 		System.out.println("Meu nome é "+ nome);
 		System.out.println("Minha idade é  "+ idade);
+		
+		System.out.println(" Valor da soma "+ soma);
 		
 		
 		
