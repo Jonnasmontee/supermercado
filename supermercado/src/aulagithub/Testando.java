3c157c7f963678344451a1668f7f9af2961a43a9
Aluno <Aluno@D095289LABIN005>
2021-12-01 21:12:29 -0300

    Criado comando para aparecer nome e idade

--- supermercado/src/aulagithub/Testando.java
+++ supermercado/src/aulagithub/Testando.java
@@ -3,13 +3,17 @@ package aulagithub;
 public class Testando {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		
 		
 		System.out.println("Alterando o arquivo");
 		
+		String nome = "Jonas Monte da Silva" ;
+		int idade = 20;
 		
+		System.out.println("Meu nome é "+ nome);
+		System.out.println("Minha idade é  "+ idade);
 		
 		
 		
